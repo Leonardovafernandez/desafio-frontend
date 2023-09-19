2f74e359610939edd63dfa35c822611f1d6dffeb
unknown <leonardovafernandez@gmail.com>
2023-09-19 18:53:43 -0300

    Feat: Implementação do Toastfy.

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -5,15 +5,30 @@ type Props = {
     classname: string;
 }
 
+export function toastSuccess({msg, classname}: Props) {
+  toast.success(`${msg}`, {
+    icon: false,
+    position: "bottom-left",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+    theme: "colored",
+    className: classname,
+  });
+}
+
 export function toastFailWhite({msg, classname}: Props) {
     toast.error(`${msg}`, {
       closeButton: false,
       position: "bottom-left",
-      autoClose: 3000,
+      autoClose: 5000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: false,
+      theme: 'light',
       className: classname,
     });
   }
